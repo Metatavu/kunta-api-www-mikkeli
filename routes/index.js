156ee2e59424eb49c2e71fae91bd8983dd234976
Antti Leppä <antti.leppa@foyt.fi>
2016-09-06 17:11:32 +0300

    Added event data into index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@
   module.exports = function(app, modules) {
   
     app.get('/', function(req, res) {
-      res.render('pages/index.pug', { });
+      
+      modules
+        .events.latest(3)
+        .callback(function (data) {
+          console.log(data[0]);
+          
+          res.render('pages/index.pug', { 
+            events: data[0]
+          });
+        }, function (err) {
+          console.log(err);
+          res.status(500).send(err);
+        });
     });
     
   };
