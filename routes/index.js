4cb2319e0486732edc57e2eaadf418166b945bc7
Antti Leppä <antti.leppa@foyt.fi>
2016-09-07 08:22:58 +0300

    Changed index events to use slider

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,29 @@
   
   var moment = require('moment');
   
+  var eventPages = 3;
+  var eventsPerPage = 3;
+  
   module.exports = function(app, modules) {
   
     app.get('/', function(req, res) {
       
       modules
-        .events.latest(3)
+        .events.latest(eventPages * eventsPerPage)
         .callback(function (data) {
           var events = (data[0]||[]).map(event => {
             return Object.assign(event, {
               "shortDate": moment(event.start).format("D/M")
             });
           });
+          
+          var eventPages = [];
+          while (events.length) {
+            eventPages.push(events.splice(0, eventsPerPage));
+          }
 
           res.render('pages/index.pug', { 
-            events: events
+            eventPages: eventPages
           });
          
         }, function (err) {
