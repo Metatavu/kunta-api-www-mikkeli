21e4cb26b925cf3b9484e55d45a6e91f903251cc
Antti Leppä <antti.leppa@foyt.fi>
2016-09-12 15:35:17 +0300

    Added initial draft of index banner

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,11 @@
       modules
         .events.latest(EVENT_PAGES * EVENTS_PER_PAGE)
         .news.latest(0, 9)
+        .banners.list()
         .callback(function (data) {
           var events = _.clone(data[0]||[]).map(event => {
             return Object.assign(event, {
               "shortDate": moment(event.start).format("D/M")
-            });
-          });
-
-          var news = _.clone(data[1]).map(newsArticle => {
-            return Object.assign(newsArticle, {
-              "shortDate": moment(newsArticle.published).format("DD.MM.YYYY")
             });
           });
           
@@ -31,8 +26,21 @@
             eventPages.push(events.splice(0, EVENTS_PER_PAGE));
           }
 
+          var news = _.clone(data[1]).map(newsArticle => {
+            return Object.assign(newsArticle, {
+              "shortDate": moment(newsArticle.published).format("DD.MM.YYYY")
+            });
+          });
+
+          var banners = _.clone(data[2]||[]).map(banner => {
+            return Object.assign(banner, {
+              imageSrc: banner.imageSrc ? banner.imageSrc : '/gfx/layout/mikkeli-banner-default.jpg'
+            });
+          });
+          
           res.render('pages/index.pug', { 
             eventPages: eventPages,
+            banners: banners,
             news: {
               top: news.splice(0, 1)[0],
               thumbs: news.splice(0, 4),
