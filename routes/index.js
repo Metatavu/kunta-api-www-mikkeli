f52110b5e8b4491eb7df854c464692e7e5506310
Antti Leppä <antti.leppa@metatavu.fi>
2016-09-28 11:39:59 +0300

    Drafting content page functionality

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,31 @@
   
   module.exports = function(app, config, ModulesClass) {
   
-    app.get('/', function(req, res) {
+    app.use(function (req, res, next) {
       var modules = new ModulesClass(config);
       
-      modules
+      modules.menus.list()
+       .callback(function (data) {
+          req.kuntaApi = {
+            data: {
+              menus: data[0]
+            }
+          };
+          
+          next();
+       }, function (err) {
+       	  console.error(err);
+          res.status(500).send(err);
+       });
+    });
+  
+    app.get('/', function(req, res) {
+      new ModulesClass(config)
         .events.latest(EVENT_PAGES * EVENTS_PER_PAGE)
         .news.latest(0, 9)
         .banners.list()
         .tiles.list()
         .socialMedia.latest(SOCIAL_MEDIA_POSTS)
-        .menus.list()
         .callback(function (data) {
           var events = _.clone(data[0]||[]).map(event => {
             return Object.assign(event, {
@@ -55,8 +70,6 @@
             });
           });
           
-          var menus = data[5]||{};
-          
           res.render('pages/index.pug', { 
             eventPages: eventPages,
             banners: banners,
@@ -67,9 +80,34 @@
               thumbs: news.splice(0, 4),
               texts: news
             },
-            menus: menus
+            menus: req.kuntaApi.data.menus
           });
          
+        }, function (err) {
+          console.error(err);
+          res.status(500).send(err);
+        });
+    });
+  
+    app.get('/sisalto*', function(req, res) {
+      var path = req.path.substring(9);
+      
+      new ModulesClass(config)
+        .pages.findByPath(path, req.headers['accept-language'])
+        .callback(function (data) {
+          var page = data[0];
+          if (!page) {
+            res.status(404).send("Not Found");
+            return;
+          }
+         
+          res.render('pages/contents.pug', {
+            title: page.title,
+            contents: page.contents,
+            featuredImageSrc: page.featuredImageSrc,
+            menus: req.kuntaApi.data.menus
+          });
+          
         }, function (err) {
           console.error(err);
           res.status(500).send(err);
