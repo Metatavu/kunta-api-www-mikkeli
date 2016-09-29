9485d5fd5b475082eed884698adc378e9196ff93
Antti Leppä <antti.leppa@metatavu.fi>
2016-09-29 09:56:58 +0300

    Fixed issue that caused social media feed tiles to overlap sometimes

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,17 @@
 
   $(document).ready(function () {
     
-    $('.social-media-items').masonry({
-      itemSelector: '.social-media-item',
-      columnWidth: '.social-media-items-grid-sizer',
-      percentPosition: true
+    $('.social-media-items').css('opacity', '0');
+    
+    $('.social-media-items').imagesLoaded(function () {
+      $('.social-media-items')
+        .css('opacity', '1')
+        .masonry({
+          itemSelector: '.social-media-item',
+          columnWidth: '.social-media-items-grid-sizer',
+          percentPosition: true
+        });
     });
     
   });
-
 }).call(this);
