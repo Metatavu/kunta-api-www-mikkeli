653e28b51578704ec34f84398b7e09acb537ed63
Antti Leppä <antti.leppa@metatavu.fi>
2017-01-11 05:46:30 +0200

    Added a page for displaying announcements

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,11 @@
   const EVENT_COUNT = 9;
   const JOB_COUNT = 5;
   const ANNOUNCEMENT_COUNT = 5;
+  const ANNOUNCEMENT_COUNT_PAGE = 10;
   const SOCIAL_MEDIA_POSTS = 4 * 3;
   const CONTENT_FOLDER = '/sisalto';
   const NEWS_FOLDER = '/uutiset';
+  const ANNOUNCEMENTS_FOLDER = '/kuulutukset';
 
   function resolveLinkType(link) {
     if (!link || link.startsWith('#')) {
@@ -229,7 +231,8 @@
       var slug = req.params.slug;
 
       if (!slug) {
-        res.status(400).send('slug is missing');
+        res.status(404).send('Not found');
+        return;
       }
 
       new ModulesClass(config)
@@ -255,6 +258,44 @@
             bannerSrc: bannerSrc,
             siblings: siblings,
             breadcrumbs : [{path: util.format('%s/%s', NEWS_FOLDER, newsArticle.slug), title: newsArticle.title }]
+          });
+
+        }, function(err) {
+          console.error(err);
+          res.status(500).send(err);
+        });
+    });
+
+    app.get(util.format('%s/:slug', ANNOUNCEMENTS_FOLDER), function(req, res) {
+      var slug = req.params.slug;
+
+      if (!slug) {
+        res.status(404).send('Not found');
+        return;
+      }
+      
+        new ModulesClass(config)
+        .announcements.list(ANNOUNCEMENT_COUNT_PAGE, 'PUBLICATION_DATE', 'DESCENDING')
+        .announcements.findBySlug(slug)
+        .callback(function(data) {
+          var announcement = data[1];
+          var siblings = data[0];
+          if (!announcement) {
+            res.status(404).send("Not Found");
+            return;
+          }
+          // TODO: Banner should come from API
+          var bannerSrc = '/gfx/layout/mikkeli-page-banner-default.jpg';
+
+           res.render('pages/announcement.pug', {
+            id: announcement.id,
+            slug: announcement.slug,
+            title: announcement.title,
+            contents: processPageContent('/', announcement.contents),
+            menus: req.kuntaApi.data.menus,
+            bannerSrc: bannerSrc,
+            siblings: siblings,
+            breadcrumbs : [{path: util.format('%s/%s', ANNOUNCEMENTS_FOLDER, announcement.slug), title: announcement.title }]
           });
 
         }, function(err) {
