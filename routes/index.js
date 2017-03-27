d44220790fb551b0b32d474cbb43e26314c808c7
Antti Leppä <antti.leppa@metatavu.fi>
2017-03-27 17:40:54 +0300

    Moved redirect routes into separate module

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,7 @@
 (function() {
   'use strict';
 
-  const util = require('util');
-  const _ = require('lodash');
-  const Common = require(__dirname + '/common');
-
   module.exports = function(app, config, ModulesClass) {
-    
-    app.get('/redirect/:type/:id', (req, res, next) => {
-      var type = req.params.type;
-      var id = req.params.id;
-      
-      switch (type) {
-        case 'page':
-          new ModulesClass(config)
-            .pages.resolvePath(id)
-            .callback((data) => {
-              if (data) {
-                res.redirect(util.format("%s/%s", Common.CONTENT_FOLDER, data));  
-              } else {
-                res.redirect('/');
-              }
-            });
-        break;
-        case 'file':
-          res.redirect(util.format("%s/%s", Common.FILES_FOLDER, id));
-        break;
-        default:
-          next({
-            status: 400,
-            message: 'Invalid type'
-          });
-        break;
-      }
-    });
     
     // Register common data middleware
     
@@ -54,6 +22,7 @@
     require(__dirname + '/announcements')(app, config, ModulesClass);
     require(__dirname + '/news')(app, config, ModulesClass);
     require(__dirname + '/search')(app, config, ModulesClass);
+    require(__dirname + '/redirect')(app, config, ModulesClass);
     
     // Register error routes. Keep these as last to ensure catch all functionality
     
---
+++ routes/redirect/index.js
@@ -0,0 +1,40 @@
+/*jshint esversion: 6 */
+(function() {
+  'use strict';
+
+  const util = require('util');
+  const Common = require(__dirname + '/../common');
+
+  module.exports = (app, config, ModulesClass) => {
+
+    app.get('/redirect/:type/:id', (req, res, next) => {
+      var type = req.params.type;
+      var id = req.params.id;
+      
+      switch (type) {
+        case 'page':
+          new ModulesClass(config)
+            .pages.resolvePath(id)
+            .callback((data) => {
+              if (data) {
+                res.redirect(util.format("%s/%s", Common.CONTENT_FOLDER, data));  
+              } else {
+                res.redirect('/');
+              }
+            });
+        break;
+        case 'file':
+          res.redirect(util.format("%s/%s", Common.FILES_FOLDER, id));
+        break;
+        default:
+          next({
+            status: 400,
+            message: 'Invalid type'
+          });
+        break;
+      }
+    });
+    
+  };
+
+}).call(this);
