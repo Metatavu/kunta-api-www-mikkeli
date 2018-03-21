d3c4d54817ad5fbc81da7731a4466fc0e67a0d32
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-03-21 14:27:41 +0200

    Added loading screen

--- public/js/mikkelin-tarina.js
+++ public/js/mikkelin-tarina.js
@@ -46,6 +46,7 @@
     $(metaform).metaform('option', 'animation.show.options.direction', delta > 0 ? 'right' : 'left');
     var page = getMetaformPage(metaform) + delta;
     if (page > getMetaformPageCount($(metaform))) {
+      $('.loading-screen').show();
       saveMetaformReply(metaform, function(res, values) {
         window.location.href = '/mikkelin-tarina?q=' + btoa(JSON.stringify(values));
       });
@@ -58,7 +59,7 @@
   function createPagedMetaform(metaform) {
     var pageCount = getMetaformPageCount(metaform);
     var pages = $('<div class="row"><div class="col text-center"><div class="metaform-pages-container"><span class="metaform-page">1</span><span>/</span><span class="metaform-pages">' + pageCount + '</span></div></div></div>');
-    var navigation = $('<div class="row"><div class="col text-right"><a class="metaform-prev" href="#"><i class="fa fa-chevron-left" aria-hidden="true"></i></a></div><div class="col text-left"><a class="metaform-next" href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div></div><input type="hidden" name="page-count" value="' + pageCount + '"/>');
+    var navigation = $('<div class="row next-prev-button-container"><div class="col text-right"><a class="metaform-prev" href="#"><i class="fa fa-chevron-left" aria-hidden="true"></i></a></div><div class="col text-left"><a class="metaform-next" href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div></div><input type="hidden" name="page-count" value="' + pageCount + '"/>');
     
     metaform.closest('.metaform-container')
       .prepend(pages)
@@ -93,6 +94,7 @@
 
     $(document).on('change', '.form-check-input', function (event) {
       event.preventDefault();
+      $('body').css('height', $('body').height() + 'px');
       $('.metaform-next,.metaform-prev').hide();
       var input = $(event.target);
       var metaform = input.closest('.metaform');
@@ -110,6 +112,7 @@
   
   $(document).on('click', '.metaform-next,.start-query-btn', function (event) {
     event.preventDefault();
+    $('body').css('height', $('body').height() + 'px');
     $('.metaform-next,.metaform-prev').hide();
     var link = $(event.target);
     changeMetaformPage(link.closest('.metaform-container').find('.metaform'), +1);
@@ -117,12 +120,14 @@
 
   $(document).on('click', '.metaform-prev', function (event) {
     event.preventDefault();
+    $('body').css('height', $('body').height() + 'px');
     $('.metaform-next,.metaform-prev').hide();
     var link = $(event.target);
     changeMetaformPage(link.closest('.metaform-container').find('.metaform'), -1);
   });
   
   $('.metaform-container').on('afterShow', function() {
+    $('body').css('height', 'auto');
     if (getMetaformPage($('.metaform-container')) > 0) {
       $('.metaform-next,.metaform-prev').show();
     }
