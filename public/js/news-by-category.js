3a1db5bb0c6944c86a9c95bc0385ab661c943c64
villekoivukangas <villekoivukangas@172.20.10.6>
2018-05-31 08:20:51 +0300

    Removed useless widget

--- public/js/news-by-category.js
+++ public/js/news-by-category.js
@@ -60,39 +60,6 @@
     }
   });
   
-  $.widget("custom.lazyBackgroundImage", {
-    
-    _create : function() {
-      this._preloaderImage = new Image();
-      this._preloaderImage.onload = $.proxy(this._onPreloadImageLoad, this);
-      this._preloaderImage.src = this._resolveUrl();
-    },
-    
-    _resolveSize: function () {
-      var imageSizeAttr = this.element.attr('data-lazy-bg-size');
-      if (imageSizeAttr) {
-        if ($.isNumeric(imageSizeAttr)) {
-          return parseInt(imageSizeAttr);
-        } else {
-          return Math.round(Math.max($(imageSizeAttr).width(), $(imageSizeAttr).height()));
-        }
-      }
-      
-      return Math.round(Math.max(this.element.width(), this.element.height()));
-    },
-  
-    _resolveUrl: function () {
-      return this.element.attr('data-lazy-bg-image') + '?size=' + this._resolveSize();
-    },
-    
-    _onPreloadImageLoad: function () {
-      this.element.css({
-        'background-image': 'url(' + this._preloaderImage.src + ')'
-      })
-      .removeAttr('data-lazy-bg-image');
-    }
-  });
-  
   $(document).ready(function () {
     $('.kunta-api-news-by-category').newsByCategory();
   });
